7b16cd948389d0537420f95c2a5fa1898c65e8b3
Black-Tomcat <tomcat.frantz@gmail.com>
2020-04-29 17:01:47 +1000

    refactored to the correct paths

--- pi/master_pi.py
+++ pi/master_pi.py
@@ -1,9 +1,9 @@
 from flask import Flask
 from flask_restful import Api
-from pi.agent import Home as Home
-from pi.agent import Register as Register
-from pi.agent import Auth as Auth
-from pi.agent import Logout as Logout
+from pi.master.home import Home as Home
+from pi.master.register import Register as Register
+from pi.master.auth import Auth as Auth
+from pi.master.logout import Logout as Logout
 
 app = Flask(__name__)
 api = Api(app)
