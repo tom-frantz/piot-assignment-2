efd70984dac16191b1e403a6b6ad6be1aa79da0d
Black-Tomcat <tomcat.frantz@gmail.com>
2020-04-29 17:05:21 +1000

    one last fix

--- pi/master_pi.py
+++ pi/master_pi.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_restful import Api
-from pi.master.test import Home as Home
+from pi.master.home import Home as Home
 from pi.master.register import Register as Register
 from pi.master.auth import Auth as Auth
 from pi.master.logout import Logout as Logout
