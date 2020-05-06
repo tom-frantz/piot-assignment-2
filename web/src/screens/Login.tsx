de77a63874a738cb4d1a4104b7d9bf36df120776
Black-Tomcat <tomcat.frantz@gmail.com>
2020-05-06 12:38:56 +1000

    Make login Ui

--- web/src/screens/Login.tsx
+++ web/src/screens/Login.tsx
@@ -1,11 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Button, Form, Input } from "antd";
 
 interface LoginProps {
     setAuth(auth: undefined | string): void;
 }
 
 const Login: React.FC<LoginProps> = (props: LoginProps) => {
-    return null;
+    const [form] = Form.useForm();
+
+    const onFinish = (values: { username: string; password: string }): void => {};
+
+    return (
+        <Form form={form} onFinish={onFinish as (values: unknown) => void} style={{ padding: 13 }}>
+            <Form.Item
+                name={"username"}
+                label={"Username"}
+                rules={[{ required: true, message: "Username is required" }]}
+            >
+                <Input />
+            </Form.Item>
+            <Form.Item
+                label={"Password"}
+                name={"password"}
+                rules={[{ required: true, message: "Password is required" }]}
+                hasFeedback
+            >
+                <Input.Password />
+            </Form.Item>
+            <Form.Item>
+                <Button type="primary" htmlType="submit">
+                    Submit
+                </Button>
+            </Form.Item>
+        </Form>
+    );
 };
 
 export default Login;
